fa327814f71365968d79efa3e778af1bff59dd5e
Priyank <Priyank@192.168.0.4>
2021-04-10 15:02:38 +0530

    Changes

--- Selenium_Testing/src/demo_test/test3.java
+++ Selenium_Testing/src/demo_test/test3.java
@@ -20,12 +20,13 @@ public class test3
 		// TODO Auto-generated method stub
 		
 		 ////////////////////////////////////////////////////////////////////////////////////////
-		/* Web Page Title verification*/
+		/* Web Home Page Title verification*/
 		 ////////////////////////////////////////////////////////////////////////////////////////
 		
 		System.setProperty("webdriver.edge.driver", "C:\\selenium\\msedgedriver.exe");
 		
-		System.out.println("\nWeb tesing for page title.....\n\n");
+		System.out.println("\nWeb tesing for home page title.....\n\n");
+		Thread.sleep(7000);
 		
 		WebDriver driver = new EdgeDriver();
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
@@ -38,33 +39,37 @@ public class test3
 		Thread.sleep(7000);
 		//Maximizing window size
 		driver.manage().window().maximize();  
+		driver.manage().deleteAllCookies();
 		
 		String actualTitle = driver.getTitle();
 		String expectedTitle = "The Sparks Foundation | Home";
 		
 		System.out.println("\nActual Title : "+actualTitle);
 		System.out.println("\nExpected Title : "+expectedTitle);
-				
+		Thread.sleep(2000);
+		
+		driver.close();
+		
+		Thread.sleep(2000);
 		if(actualTitle.equals(expectedTitle))
 		{
-			//System.out.println("\nThe Title of the page is : "+actualTitle);
 			System.out.println("\nTittle is Verified");
+			//Thread.sleep(7000);
 		}
 		else
 		{
-			//System.out.println("\nThe Title of the page is : "+actualTitle);
 			System.out.println("\nVerification failed !!!");
 		}
 		
 		Thread.sleep(7000);
-        driver.close();
         
+                
         ////////////////////////////////////////////////////////////////////////////////////////
-		/* Web Url Verification */
+		/* Web Home Page Url Verification */
         ///////////////////////////////////////////////////////////////////////////////////////
         
-        System.out.println("\nWeb tesing for page title.....\n\n");
-        
+        System.out.println("\nWeb tesing for GRIP(Internship) page url .....\n\n");
+        Thread.sleep(2000);
         
         WebDriver driver1 = new EdgeDriver();
 		driver1.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
@@ -76,29 +81,77 @@ public class test3
 		Thread.sleep(7000);
 		//Maximizing window size
 		driver1.manage().window().maximize();
+		driver.manage().deleteAllCookies();
 		
 		//Click on link 'About Us' using cssSelector
-		driver1.findElement(By.cssSelector("#link-effect-3 > ul > li:nth-child(6) > a")).click(); 
+		driver1.findElement(By.partialLinkText("GRIP")).click(); 
 		
-		String actualUrl = driver1.getCurrentUrl();
-		String expectedUrl = "https://www.thesparksfoundationsingapore.org/contact-us/";
+		String actualP1Url = driver1.getCurrentUrl();
+		String expectedP1Url = "https://www.thesparksfoundationsingapore.org/join-us/internship-positions/";
 		
-		System.out.println("\nActual Url : "+actualUrl);
-		System.out.println("\nExpected Url : "+expectedUrl);
+		System.out.println("\nActual Url : "+actualP1Url);
+		System.out.println("\nExpected Url : "+expectedP1Url);
+		Thread.sleep(2000);
+		driver1.close();
 		
-		if(actualUrl.equals(expectedUrl))
+		Thread.sleep(2000);
+		if(actualP1Url.equals(expectedP1Url))
 		{
-			//System.out.println("\n\nThe Url of the page is : "+actualUrl);
-			System.out.println("\nUrl is verified");
+			System.out.println("\nGRIP(Internship) page Url is verified");
+			//Thread.sleep(7000);
 		}
 		else
 		{
-			//System.out.println("\nThe Url of the page is : "+actualUrl);
 			System.out.println("\nVerification failed !!!");
 		}
 						
 		Thread.sleep(7000);
-        driver1.close();
+        
+        
+        ////////////////////////////////////////////////////////////////////////////////////////
+		/* Web Contact Us page Url Verification */
+        ///////////////////////////////////////////////////////////////////////////////////////
+        
+        System.out.println("\nWeb tesing for page title on 'Contact Us' Page.....\n\n");
+        Thread.sleep(7000);
+        
+        WebDriver driver2 = new EdgeDriver();
+		driver2.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver2.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		//Navigating to the site
+		driver2.navigate().to("https://www.thesparksfoundationsingapore.org/");
+				
+		Thread.sleep(7000);
+		//Maximizing window size
+		driver2.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		
+		//Click on link 'About Us' using cssSelector
+		driver2.findElement(By.cssSelector("#link-effect-3 > ul > li:nth-child(6) > a")).click(); 
+		
+		String actualUrl = driver2.getCurrentUrl();
+		String expectedUrl = "https://www.thesparksfoundationsingapore.org/contact-us/";
+		
+		System.out.println("\nActual Url : "+actualUrl);
+		System.out.println("\nExpected Url : "+expectedUrl);
+		Thread.sleep(2000);
+		
+		driver2.close();
+		
+		Thread.sleep(2000);
+		if(actualUrl.equals(expectedUrl))
+		{
+			System.out.println("\nUrl is verified on the Contact Us Page");
+			//Thread.sleep(7000);
+		}
+		else
+		{
+			System.out.println("\nVerification failed !!!");
+		}
+						
+		Thread.sleep(7000);
+        
 		
 
 	}
